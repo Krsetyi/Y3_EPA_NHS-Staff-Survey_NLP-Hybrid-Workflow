1326896df2474d1c884907cd1d0bc3b35a1cc8df
Krsetyi <kyrstiej86@gmail.com>
2026-04-29 13:53:06 +0100

    Update utils.py

--- pipeline/utils.py
+++ pipeline/utils.py
@@ -1,1 +1,8 @@
+import logging
+logging.basicConfig(level=logging,INFO)
+logger = logging.getLogger(__name__)
 
+from .utils import logger
+def run_preprocessing(years: list[int]):
+  logger info("Loading data for years: %s", years)
+  ...
