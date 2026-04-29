c772608286ceab9830b028321114a20164da690e
Krsetyi <kyrstiej86@gmail.com>
2026-04-29 13:44:16 +0100

    Update metadata.py

--- pipeline/metadata.py
+++ pipeline/metadata.py
@@ -1,1 +1,8 @@
+Goal: Add derived fields that will be useful later (and for analysis)
 
+import pandas as pd
+def add_metadata(df: pd.DataFrame) -> pd.DataFrame:
+  df = df.copy()
+  df["comment_length"] = df["comment_clean"].str.len()
+  df["has_text"] = df["comment_length"] > 0
+  return df
