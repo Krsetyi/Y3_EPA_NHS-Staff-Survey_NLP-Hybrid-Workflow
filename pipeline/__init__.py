0a608938f33797b0de4e3d4ed1dc2265ce565dff
Krsetyi <kyrstiej86@gmail.com>
2026-06-11 15:20:41 +0100

    Update __init__.py

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,3 +1,6 @@
-from .utils import run_preprocessing
+init_path = "/content/Y3_EPA_NHS-Staff-Survey_NLP-Hybrid-Workflow/pipeline/__init__.py"
 
+with open(init_path, "w") as f:
+    f.write("from .utils import run_preprocessing\n")
 
+print("__init__.py overwritten CLEANLY.")
